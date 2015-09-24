e1f4422ab8cc453db81ae95cde10ad791bcb769c
Caio SBA <caiosba@gmail.com>
2015-09-24 11:38:06 -0300

    Initial commit: Clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twittexplorer::Application.config.secret_key_base = 'f7bacce4e3e7839ba9ea06b502033f80ef9ae6d5e6248ea08efe86a8d15978a3e9bf183414d7eea40a49cb2ce7948dc1a908ea025e9df3619e3d1a783888746b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twittexplorer::Application.config.session_store :cookie_store, key: '_twittexplorer_session'
